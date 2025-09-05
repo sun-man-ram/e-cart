f504a5cf678a86dbe4b558474611e544a7b04d0b
sun-man-ram <manikantasunkara2653@gmail.com>
2025-09-05 20:19:26 +0530

    database setup done

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 
-
+import {connectDB} from "./lib/db.js";
 
 
 dotenv.config(); 
@@ -14,5 +14,6 @@ app.use("/api/auth",authRoutes);
 
 app.listen(PORT,()=>{
     console.log("Server is running on "+PORT);
+    connectDB();
 });
 
